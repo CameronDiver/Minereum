a33b95fc381ce46c63c4ef9a529cb4c77b549da9
Cameron Diver <cameron.diver94@gmail.com>
2015-05-19 21:43:43 +0100

    Added command line argument support to the script

--- runminer.py
+++ runminer.py
@@ -2,11 +2,76 @@
 
 from subprocess import Popen, PIPE
 import sys, time, os, signal, numpy
+import getopt
 
 # hammer hexcode 01f528 Mined block block number [maybe not that order]
 
 OUTPUT_DELAY = 2
 SPEED_MAX_SAMPLE_SIZE = 100
+
+DEFAULTS = {
+    'GPU': False,
+    'benchmark': False,
+    'run-server' : False,
+    'geth-server': 'geth',
+    'ethminer': 'ethminer',
+    'output-poll-time': 2
+}
+
+OPTIONS = (
+    "hGMst:",
+    [
+        'geth='
+        'ethminer='
+        'help'
+    ]
+)
+
+def printUsage():
+    print 'runminer.py - Ethereum mining helper script using ethminer and geth.'
+    print 'Usage: ./runminer.py [-G] [-M] [-s] [-t n] [--geth-server=] [--ethminer=]'
+    print '\t -G \t Run on the graphics card'
+    print '\t -M \t Perform a benchmark test using ethminer'
+    print '\t -s \t Run Geth server before executing ethminer'
+    print '\t -t n \t Wait n seconds before displaying output (default: 2)'
+    print '   --geth-server=    indicates the location of the geth binary'
+    print '   --ethminer=       indicates the location of the ethminer binary'
+
+def getOptions(args):
+    config = {}
+
+    try:
+        opts, args = getopt.getopt(args, OPTIONS[0], OPTIONS[1])
+    except getopt.GetoptError:
+        print 'Error parsing input'
+        printUsage()
+        sys.exit()
+
+    for opt, arg in opts:
+        if opt == '-G':
+            config['GPU'] = True
+        elif opt == '-M':
+            config['benchmark'] = True
+        elif opt == '-s':
+            config['run-server'] = True
+        elif opt == '-t':
+            config['output-poll-time'] = int(arg)
+        elif opt == '--geth':
+            config['geth-server'] = arg
+        elif opt == '--ethminer':
+            config['ethminer'] = arg
+        elif opt in ('-h', '--help'):
+            printUsage()
+            sys.exit()
+    
+    # Set any unset values to the default
+    for key in DEFAULTS:
+        if not key in config:
+            config[key] = DEFAULTS[key]
+
+    return config
+
+
 
 def readableHash(hps):
     prefixes = ['h', 'Kh', 'Mh', 'Gh', 'Th', 'Ph']
@@ -22,22 +87,26 @@ def rollingAverage(speedList):
     # TODO: rolling average
     return numpy.mean(speedList)
 
-
+config = getOptions(sys.argv[1:])
 
 command = [
-    'ethminer',
+    config['ethminer']
 ]
 
-if len(sys.argv)< 2:
-    print 'Running on CPU'
-else:
-    if sys.argv == '-G':
-        print 'Sorry, only -G switch supported'
-        sys.exit()
-    print 'Running on GPU'
+if config['GPU']:
     command.append('-G')
+    print 'Running on GPU'
+if config['benchmark']:
+    print 'Running benchmarking test'
+    command.append('-M')
+
+# TODO: run server
 
 print "Running command '%s'" % ' '.join(command)
+print config
+
+
+sys.exit()
 
 process = Popen(command, stderr=PIPE)
 
